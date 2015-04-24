fbcd1708358305cee8097c4827416cdcd66babb2
asoldano <asoldano@50344849-fb0f-0410-a53f-a94597aac05c>
2015-04-24 19:38:06 +0000

    Tagging jaxws-codefirst-1.1.0.Final maven archetype

---
+++ projects/plugins/maven/archetypes/jaxws-codefirst/tags/jaxws-codefirst-1.1.0.Final/src/main/resources/archetype-resources/src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+package ${package};
+
+import javax.jws.WebService;
+
+@WebService(targetNamespace = "http://hello.world.ns/")
+public interface HelloWorld {
+    String sayHi(String text);
+    String greetings(Person person);
+}
+
